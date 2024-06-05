eab19b38450d91bf3a603de26db808d4c56ecaf5
Md. Monirujjaman <threepolesoft>
2024-06-05 18:29:55 +0600

    loader added, change background on login screen

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -8,3 +8,27 @@ function getToken() {
     return document.getElementsByTagName("__RequestVerificationToken").value;
 
 }
+
+function ButtonLoaderShow(selector) {
+
+    $(selector).append("<img src='/assets/images/loader.gif' style='height: 25px; width: 25px;margin-left: 10px;'/>");
+
+}
+
+function ButtonLoaderHide(selector) {
+
+    $(selector + " img").remove();
+
+}
+
+function LoaderOneShow(selector) {
+
+    $(selector).html("<div style='text-align: center'><img src='/assets/images/loader.gif'/> </div>");
+
+}
+
+function LoaderOneHide(selector) {
+
+    $(selector).html("");
+
+}
